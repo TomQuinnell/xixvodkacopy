d30f465b80f07cbec08e1cbbdecbc5befcc8716b
tomquin456@gmail.com <tomquin456@gmail.com>
2023-01-05 16:50:20 +0000

    Add spinning bottle bg

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
+  private static URL ="https://d11am8ji68y4wp.cloudfront.net/Desktop_WebP/Bottle_Animation_WEB_1080_";
+
+  public webURL = "";
+  public yPrev: number;
+
+  public animate: boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.yPrev = 0;
+    this.updateBGContent();
   }
 
+  range(start: number, end: number, step: number): number[] {
+    return Array.from(Array(Math.round((end - start + 1) / 5)).keys()).map(x => x * step + start);
+  }
+
+  @HostListener("window:scroll", [])
+  private updateBGContent() {
+    if (!this.animate) {
+      return;
+    }
+    const y = window.scrollY;
+    // Change the background image
+    this.range(Math.min(this.yPrev, y), Math.max(this.yPrev, y), 5).forEach((i: number) => {
+      const label = Math.min(Math.floor(i/20) + 1, 290);
+      const imageURL = HomePageComponent.URL + HomePageComponent.addLeadingZeros(label) + '.webp';
+      console.log(i);
+      console.log(imageURL);
+      this.setBGContent(imageURL);
+    })
+    this.yPrev = y;
+  }
+
+  private setBGContent(url: string) {
+    this.webURL = url;
+  }
+
+  private static addLeadingZeros(num: number, totalLength: number=4): string {
+    return String(num).padStart(totalLength, '0');
+  }
 }
